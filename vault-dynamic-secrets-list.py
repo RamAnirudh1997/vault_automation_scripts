eb5a54f31e900cc50892d27afa359720aa85a4cc
Ram Anirudh Jayasankar <ramanirudhjayasankar@Rams-MacBook-Pro.local>
2025-07-30 21:38:48 -0400

    testing 2  git

--- vault-dynamic-secrets-list.py
+++ vault-dynamic-secrets-list.py
@@ -3,6 +3,7 @@ import csv
 #I want to test git
 # Vault Configuration
 VAULT_ADDR = "https://your-vault-instance:8200"
+try there
 VAULT_TOKEN = "your-vault-token"
 NAMESPACE = "admin"  # Namespace to pass
 
